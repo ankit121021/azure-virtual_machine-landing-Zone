0f53c5ff8a42578a74d95eb26fd42f897eac8efa
ankit121021 <ankitkumar903993@gmail.com>
2026-08-13 23:13:52 +0530

    cleaned up vnet output.tf

--- modules/vnet/output.tf
+++
@@ -1,9 +0,0 @@
-output "vnet_output" {
-  value = {
-    for k, v in azurerm_virtual_network.vnet : k => {
-      id                  = v.id
-      name                = v.name
-      resource_group_name = v.resource_group_name
-    }
-  }
-}
